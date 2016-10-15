5cdd4cd1f03cecd98562879eec2c9dbbef4a04eb
ToxicTree <zoi009900@gmail.com>
2016-10-15 15:02:30 +0200

    Added a example table to be ready after install

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        $this->call(ExampleTableSeeder::class);
+    }
+
+}
+
+class ExampleTableSeeder extends Seeder {
+    public function run() {
+
+        Schema::dropIfExists('table_example');
+
+        Schema::create('table_example', function($table){
+            
+            $table->increments('id');
+            $table->text('text_column')->nullable();
+            $table->integer('integer_column')->nullable();
+
+        });
+        
+        $insert = [
+            ['text_column' => 'example text', 'integer_column' => 1 ],
+            ['text_column' => 'more text', 'integer_column' => 52 ]
+        ];
+
+        DB::table('table_example')->insert($insert);
+
     }
 }
